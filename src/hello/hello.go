fc50cc7a1867a1fee6830dba7f5693fb69617c68
cong <cong@cc>
2018-06-30 10:16:55 -0400

    hello

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,16 +2,40 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
-func main()  {
-	fmt.Println(7/5)
+type Error struct {
+	errCode uint8
+}
+
+func main() {
+	fmt.Println(7 / 5)
 	fmt.Print("Hello world!\n")
+	var err Error
+
+	fmt.Println(err)
+	println(err)
+	println(7)
+
+	//for _, env := range os.Environ(){
+	//	fmt.Println(env)
+	//}
+
+	//var e interface{}
+	//fmt.Println(&e)
+	//if e != nil {
+	//	fmt.Println("why")
+	//
+	//}
+
+	fmt.Println(f())
 
 
-	for _, env := range os.Environ(){
-		fmt.Println(env)
-	}
+}
 
+func f() (result int) {
+	defer func() {
+		result++
+	}()
+	return 0
 }
